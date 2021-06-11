879d608c015304f531343cf2c0242e41194eae95
rohitkanteti <59766432+rohitkanteti@users.noreply.github.com>
2021-06-11 19:09:34 +0530

    Update pattern5.cpp

--- Assignments/Solutions/Venkata Naga Sai Rohit Kanteti/Assignment1/pattern5.cpp
+++ Assignments/Solutions/Venkata Naga Sai Rohit Kanteti/Assignment1/pattern5.cpp
@@ -14,18 +14,11 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-void read_write()
-{
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-}
 int main()
 {
-   read_write();
+  
 
-	int i,j,k,n;
+ int i,j,k,n;
   cin>>n;
   for(i=1;i<=n;i++)
   {
