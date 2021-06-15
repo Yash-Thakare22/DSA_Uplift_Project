27d475ad01e1a51ed2dfa0508ef38f42c8e68862
Anuj Nandeshwar <78202785+AnujNandeshwar@users.noreply.github.com>
2021-06-16 02:07:18 +0530

    pattern1

--- Assignments/Solutions/Anuj Nandeshwar/Assignment1/Pattern1.java
+++ Assignments/Solutions/Anuj Nandeshwar/Assignment1/Pattern1.java
@@ -11,7 +11,7 @@ public class Pattern1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int rows = sc.nextInt();
-
+        sc.close();
         for(int i=1;i<=rows;i++){
             for(int j=1;j<=(3*(rows-i));j++){
                 System.out.print(" ");
