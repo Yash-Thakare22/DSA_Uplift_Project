979bd2d54b81f0d632cd34cd8772f818ba582997
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2021-06-16 23:54:35 +0530

    Assignment 1: Question 1 Solved

---
+++ Assignments/Solutions/Suraj Jha/Assignment 1/q1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter Number: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "   ";
+        }
+        for (int j = 0; j < n; j++)
+        {
+            cout << " * ";
+        }
+		cout << endl;
+    }
+
+    return 1;
+}
