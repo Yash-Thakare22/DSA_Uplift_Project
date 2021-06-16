b580eac1972e8de8f3f1e1a6e60a63ca44c144fe
Induja <53386868+indujaaaa@users.noreply.github.com>
2021-06-16 11:52:43 +0530

    Added Assignment 5 files

---
+++ Assignments/Solutions/Induja Shankar/Assignment 5/q4_lexico.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+	char arr[n];
+	for(int i = 0; i < n; i++)
+	    cin >> arr[i];
+
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(arr[i] > arr[j])
+                swap(arr[i], arr[j]);
+        }
+    }
+
+	for(int i = 0; i < n; i++)
+        cout << arr[i];
+	
+	return 0;
+}
