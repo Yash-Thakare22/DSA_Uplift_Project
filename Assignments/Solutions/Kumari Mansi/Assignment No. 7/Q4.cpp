f984a90abe302ad8f731e7761ff48f47c143db89
Kumari Mansi <mansinegiuk@gmail.com>
2021-06-19 18:36:27 +0530

    Assignment No. 7 completed by Kumari Mansi

---
+++ Assignments/Solutions/Kumari Mansi/Assignment No. 7/Q4.cpp
@@ -0,0 +1,19 @@
+/*
+Question 4
+Given an integer x, write a function that multiplies x with 3.5 and returns the integer result. You are allowed to use only bitwise operator
+
+I/P:
+5 
+
+O/P:
+17
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long int n;
+    cin >> n;
+    cout << ((n << 1) + n + (n >> 1));
+    return 0;
+}
