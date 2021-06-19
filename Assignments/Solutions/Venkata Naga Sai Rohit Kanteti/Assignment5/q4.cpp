226fa6a47aa1feb16a6bb5362dcc7d1eac659354
rohitkanteti <59766432+rohitkanteti@users.noreply.github.com>
2021-06-19 17:30:26 +0530

    Create q4.cpp

---
+++ Assignments/Solutions/Venkata Naga Sai Rohit Kanteti/Assignment5/q4.cpp
@@ -0,0 +1,39 @@
+/*
+Question 4: Given a string arr consisting of lowercase english letters, arrange all its letters in lexicographical order using Counting Sort.
+
+Input:
+N = 5
+S = "edsab"
+Output:
+abdes
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define N 26
+void count_sort(char*s,int n)
+{
+ int c[N]={0};
+ for (int i=0; i<n; i++)
+    {
+    c[s[i]-'a']++;   
+    }
+     
+    for (int i=0;i<N;i++)
+        for (int j=0;j<c[i];j++)
+            cout << (char)('a'+i);
+            
+    
+}
+
+int main()
+{
+int n;
+cin>>n;
+char s[n];
+for(int i=0;i<n;i++)
+{
+cin>>s[i];
+}
+count_sort(s,n);
+return 0;
+}
