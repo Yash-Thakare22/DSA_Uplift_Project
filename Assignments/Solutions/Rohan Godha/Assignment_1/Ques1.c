80923f323920011b7852b8f5efe702d8252f2c8e
RohanGodha <68965506+RohanGodha@users.noreply.github.com>
2021-06-22 00:59:33 +0530

    Create Ques1.c

---
+++ Assignments/Solutions/Rohan Godha/Assignment_1/Ques1.c
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+
+{
+
+for(int i=5;i>0;i--)
+
+    {
+
+        int k=i;
+
+        while(k!=0)
+
+        {
+
+        cout<<" ";
+
+        k--;
+
+        }
+
+        
+
+        for(int j=0;j<5;j++)
+
+        {
+
+         cout<<"*";   
+
+        }
+
+        cout<<endl;
+
+    }
+
+    return 0;
+
+}
