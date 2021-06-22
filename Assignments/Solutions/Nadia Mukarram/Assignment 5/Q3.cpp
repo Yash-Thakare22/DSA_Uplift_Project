bce3b61c7ce99bddf8e736c1b163d340814df130
nadiamukarram <77635035+nadiamukarram@users.noreply.github.com>
2021-06-22 12:52:04 +0530

    Create Q3.cpp

---
+++ Assignments/Solutions/Nadia Mukarram/Assignment 5/Q3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int a[n], b[m];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int j = 0; j < m; j++)
+    {
+        cin >> b[j];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (a[i] == b[j])
+            {
+                cout << a[i];
+            }
+        }
+    }
+    return 0;
+}
