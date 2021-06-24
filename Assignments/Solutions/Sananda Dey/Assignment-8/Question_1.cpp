baefa76088b70ac5b13856d2840590e44722b4f6
Sananda Dey <76838248+19082002@users.noreply.github.com>
2021-06-24 18:55:31 +0530

    Add files via upload

---
+++ Assignments/Solutions/Sananda Dey/Assignment-8/Question_1.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// swap k'th element from beginning and end
+	void swapKth(int *arr, int n, int k) {
+	    int temp =arr[n-k];
+   arr[n-k]=arr[k-1];
+   arr[k-1]=temp;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.swapKth(arr, n, k);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
