783eb744dd540f27099e2eb86bbf91385274c439
Divyanshi-03 <85453778+Divyanshi-03@users.noreply.github.com>
2021-06-28 12:37:52 +0530

    Create q1.cpp

---
+++ Assignments/Solutions/Divyanshi Sharma/assignment 2/q1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin >> a[i];
+
+    }
+    for(int i=0;i<n;i++)
+    { for(int j =i+1 ;j<n;j++)
+       {
+
+        if(a[i]>a[j])
+        {
+            int temp= a[i];
+            a[i]=a[j];
+            a[j]= temp ;
+        }}
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<< a[i]<<" " ;
+
+    }
+}
