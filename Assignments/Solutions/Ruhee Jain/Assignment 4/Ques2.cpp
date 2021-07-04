76e12fb1e89cc8196177f35f7cfaa2ff87e71ed6
Ruheej1 <59756474+Ruheej1@users.noreply.github.com>
2021-07-04 15:46:48 +0530

    Update Ques2 solution

--- Assignments/Solutions/Ruhee Jain/Assignment 4/Ques2.cpp
+++ Assignments/Solutions/Ruhee Jain/Assignment 4/Ques2.cpp
@@ -10,3 +10,30 @@ O/P:
 abcd
 
 */
+
+
+#include <iostream>
+using namespace std;
+ 
+char *removeDuplicate(char str[], int n)
+{
+   int index = 0;  
+   for (int i=0; i<n; i++) {
+     int j;
+     for (j=0; j<i; j++){
+         if (str[i] == str[j])
+          break; 
+     }
+     if (j == i)
+        str[index++] = str[i];
+   }
+   return str;
+}
+
+int main()
+{
+   char str[100];
+   cin.get(str,100);
+   int n = sizeof(str) / sizeof(str[0]);
+   cout << removeDuplicate(str, n);
+}
