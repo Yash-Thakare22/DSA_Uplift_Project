48271ac9091f8ddbad7dd0d48a12fc869dce818b
Nimisha500 <64682800+Nimisha500@users.noreply.github.com>
2021-07-13 00:35:46 +0530

    Create Question 1.py

---
+++ Assignments/Solutions/Nimisha Agarwal/Assignment No.1/Question 1.py
@@ -0,0 +1,16 @@
+"""
+Question 1.
+
+             *  *  *  *  *
+          *  *  *  *  *
+       *  *  *  *  *
+    *  *  *  *  *
+ *  *  *  *  *
+
+"""
+for i in range(1,6):
+    for j in range(5,i,-1):
+        print(" ",end="  ")
+    for k in range(1,6):
+        print("*",end="  ")
+    print(end="\n")
