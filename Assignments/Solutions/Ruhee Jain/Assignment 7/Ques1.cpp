e4353f1086085af46ddc50d61e44c3a2f784f4f2
Ruhee Jain <59756474+Ruheej1@users.noreply.github.com>
2021-07-12 19:26:54 +0530

    Update Ques1.cpp

--- Assignments/Solutions/Ruhee Jain/Assignment 7/Ques1.cpp
+++ Assignments/Solutions/Ruhee Jain/Assignment 7/Ques1.cpp
@@ -11,3 +11,43 @@ O/P:
 
 */
 
+#include <iostream>
+using namespace std;
+#define INT_SIZE 32
+ 
+int getSingle(int arr[], int n)
+{
+  
+    int result = 0;
+ 
+    int x, sum;
+ 
+    for (int i = 0; i < INT_SIZE; i++) {
+ 
+        sum = 0;
+        x = (1 << i);
+        cout<<x<<endl;
+        for (int j = 0; j < n; j++) {
+            if (arr[j] & x)
+                sum++;
+        }
+ 
+        if ((sum % 3) != 0)
+            result |= x;
+    }
+ 
+    return result;
+}
+ 
+int main()
+{
+   int n;
+   cin>>n;
+   int a[n];
+   for(int i=0; i<n; i++){
+       cin>>a[i];
+   }
+    cout << "The element with single occurrence is " << getSingle(a, n);
+    return 0;
+}
+ 
