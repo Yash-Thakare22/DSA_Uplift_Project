180d5118453e3ac6722ba94ddb829a001298998d
Lavisharora30 <lavish.arora.3019@gmail.com>
2021-07-13 12:52:15 +0530

    Assignment 11 Solutions

---
+++ Assignments/Solutions/Lavish Arora/Assignment No. 11/Q6.cpp
@@ -0,0 +1,14 @@
+class Solution
+{   
+    public:  
+    //Function to find transpose of a matrix.
+    void transpose(vector<vector<int> >& matrix, int n)
+    { 
+        // code here 
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+    }
+};
