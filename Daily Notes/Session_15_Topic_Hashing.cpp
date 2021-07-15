9f7409f733f216a4612acdb17b5d5267c84724f3
Anushka Srivastava <67271376+anushka-srivastava22@users.noreply.github.com>
2021-07-15 21:03:41 +0530

    Update Session_15_Topic_Hashing.cpp

--- Daily Notes/Session_15_Topic_Hashing.cpp
+++ Daily Notes/Session_15_Topic_Hashing.cpp
@@ -1,9 +1,14 @@
+What is Hashing ?
 Hashing is the process of mapping large amount of data item to smaller table with the help of hashing function.
 Hashing is also known as Hashing Algorithm or Message Digest Function.
 Hashing allows to update and retrieve any data entry in a constant time O(1).
 Constant time O(1) means the operation does not depend on the size of the data.
 A fixed process converts a key to a hash key is known as a Hash Function.
+
+What is Hash Value ?
 This function takes a key and maps it to a value of a certain length which is called a Hash value or Hash.
+ 
+Hash Functions
 A hash function may map different  keys to the same slot. This is called a “collision”, when  two keys have the same hash value
 Good Hash Functions
 Fast to compute, O(1)
@@ -11,14 +16,21 @@ Scatter keys evenly throughout the hash table
 Less collisions
 Need less slots (space)
 
+Hash Table
+A hash table (or hash map) is a data structure  that uses a hash function to efficiently map keys  to values, for efficient search and retrieval
+
+Hash Maps / Unordered Maps
+Unordered maps are associative containers that store elements formed by the combination of a key value and a mapped value, and which allows for fast retrieval of individual elements based on their keys.
+In an unordered_map, the key value is generally used to uniquely identify the element, while the mapped value is an object with the content associated to this key. Types of key and mapped value may differ.
+ 
 Hashing of Strings: Example
 hash1("Tan Ah Teck")
 = ('T' + 'a' + 'n' + ' ' +  'A' + 'h' + ' ' +
 'T' + 'e' + 'c' + 'k') % 11
 // hash table size is 11
-= (84 + 97 + 110 + 32 + 65+ 102+32+84+101+99+107)%11
-=825%11
-=0
+= (84 + 97 + 110 + 32 + 65 + 102 + 32 + 84 + 101 + 99 + 107)%11
+= 825%11
+= 0
 
 Most Frequent Element Program
 
