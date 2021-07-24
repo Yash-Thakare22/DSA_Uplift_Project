32b92e717132cc5a1329cb2ff4617e57d7d7c010
Ruhee Jain <59756474+Ruheej1@users.noreply.github.com>
2021-07-25 00:24:31 +0530

    Ques5 solution

--- Assignments/Solutions/Ruhee Jain/Assignment 8/Ques5.cpp
+++ Assignments/Solutions/Ruhee Jain/Assignment 8/Ques5.cpp
@@ -4,3 +4,18 @@ Ques 5
 PATTERN: https://practice.geeksforgeeks.org/problems/triangle-shrinking-downwards0459/1
 
 */
+class Solution{
+  public:
+  string triDownwards(string S){
+   int len = S.length();
+        string res="";
+        for(int  i = 0; i < len; i++)
+        {
+            for(int j = 0; j < len; j++)
+            {
+                res += S[j];
+            }
+            res[i]='.';
+        }
+        return res;
+    }
