f8d6963653f473abd4150b60f0e9dc7d6da97ca9
Ruhee Jain <59756474+Ruheej1@users.noreply.github.com>
2021-07-25 00:17:54 +0530

    solution Ques6

--- Assignments/Solutions/Ruhee Jain/Assignment 8/Ques6.cpp
+++ Assignments/Solutions/Ruhee Jain/Assignment 8/Ques6.cpp
@@ -5,3 +5,29 @@ Ques 6
 BIT MANIPULATION : https://practice.geeksforgeeks.org/problems/odd-or-even3618/1 (DO IT WITH BIT MANIPULATION CONCEPT ONLY)
 
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class solution{
+public:
+    string oddEven(int N){
+        if(N&1){
+            return "odd";
+        }
+        return "even";
+    }
+};
+
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+    int N;
+    cin>>N;
+    Solution ob;
+    cout<< ob.oddEven(N) << endl;
+  }
+  return 0;
+}
+
