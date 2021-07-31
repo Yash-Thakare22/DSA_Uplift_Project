d2407a05f62133dfcd056b87df5efd3db0a6bebb
Ruhee Jain <59756474+Ruheej1@users.noreply.github.com>
2021-07-31 21:01:51 +0530

    Ques4.cpp

---
+++ Assignments/Solutions/Ruhee Jain/Assignment 11/Ques4.cpp
@@ -0,0 +1,41 @@
+/*
+
+Ques4
+
+https://practice.geeksforgeeks.org/problems/distinct-pattern-search-1587115620/1/?track=DSASP-Strings&batchId=154
+
+*/
+
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+class Solution
+{
+    public:
+    bool search(string pat, string txt) 
+    { 
+     int found = txt.find(pat);
+        while (found != string::npos) {
+            return true;   
+            found = txt.find(pat, found + 1);
+    }
+    return false;
+    } 
+};
+
+int main() 
+{ 
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    string s, p;
+	    cin >> s >> p;
+	    Solution obj;
+	    if(obj.search(p, s)) cout << "Yes"; else cout << "No"; cout << endl;
+	}
+	
+	return 0; 
+} 
