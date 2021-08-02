f8e694c1928bf357c22ec0d3ac4685a6a9707dc0
Ruhee Jain <59756474+Ruheej1@users.noreply.github.com>
2021-08-02 15:50:56 +0530

    Ques1 solution

--- Assignments/Solutions/Ruhee Jain/Assignment 16/Ques1.cpp
+++ Assignments/Solutions/Ruhee Jain/Assignment 16/Ques1.cpp
@@ -5,3 +5,34 @@ Ques1
 https://leetcode.com/problems/valid-parentheses/
 
 */
+
+class Solution {
+public:
+    bool isValid(string s) {
+     stack<char> st;
+        char x;
+        for (int i=0; i<s.length(); i++){
+           if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+           {
+               st.push(s[i]);
+               continue;
+           }  
+           if (st.empty())
+                  return false;
+           
+            if(s[i]==')' && st.top()=='('){
+                    st.pop();
+            }
+            else if(s[i]=='}' && st.top()=='{'){
+                st.pop();
+            }
+            else if(s[i]==']' && st.top()=='['){
+                st.pop();
+            }  
+            else 
+                return false;
+    }
+        return (st.empty());
+       
+    }
+};
