cc3dcb3dc3fd2ce494a1bcc20b7b5916fc5e9971
Ruhee Jain <59756474+Ruheej1@users.noreply.github.com>
2021-08-02 16:13:17 +0530

    Ques2 solution

--- Assignments/Solutions/Ruhee Jain/Assignment 16/Ques2.cpp
+++ Assignments/Solutions/Ruhee Jain/Assignment 16/Ques2.cpp
@@ -6,4 +6,25 @@ https://leetcode.com/problems/palindrome-linked-list/
 
 */
 
+class Solution {
+public:
+   bool isPalindrome(ListNode* head) {
+	ListNode *slow = head, *fast = head, *prev = NULL, *tmp;
+	while(fast && fast -> next){
+       fast = fast -> next -> next;
+	   tmp = slow -> next;
+       slow -> next = prev;
+       prev = slow;
+       slow = tmp; 
+    }
+	           
+	slow = (fast ? slow -> next : slow);
+	while(slow) 
+		if(slow -> val != prev -> val) 
+            return false;
+		else slow = slow -> next, prev = prev -> next;
+	return true;
+}
+};
+
 
