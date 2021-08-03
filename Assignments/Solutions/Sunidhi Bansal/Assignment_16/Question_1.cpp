2a5dac1eedbdd257057238cd9ed4eb414a0cec38
sunidhib2002 <agilist.sunidhi@gmail.com>
2021-08-03 17:55:53 +0530

    Assignment-16 Solutions

---
+++ Assignments/Solutions/Sunidhi Bansal/Assignment_16/Question_1.cpp
@@ -0,0 +1,35 @@
+#include <stack>
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> t;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                t.push(s[i]);
+            }
+            else {
+                if (t.size() == 0) {
+                    return false;
+                }
+                else {
+                    if (s[i] == ')' && t.top() == '(') {
+                        t.pop();
+                    }
+                    else if (s[i] == '}' && t.top() == '{') {
+                        t.pop();
+                    }
+                    else if (s[i] == ']' && t.top() == '[') {
+                        t.pop();
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            }
+        }
+        if(t.size() == 0) {
+            return true;    
+        }
+        return false;
+    }
+};
