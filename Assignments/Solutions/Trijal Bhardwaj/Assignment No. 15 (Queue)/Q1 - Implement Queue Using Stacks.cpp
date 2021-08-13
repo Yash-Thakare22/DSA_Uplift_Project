e6b3b8365e800132aca2098a9d70064cd16c3dce
Trijal-Bhardwaj <Trijalbhardwaj00@gmail.com>
2021-08-13 06:22:03 +0530

    Assignment No. 15 (Queue) Solutions By TRIJAL BHARDWAJ

--- Assignments/Solutions/Trijal Bhardwaj/Assignment No. 15 (Queue)/Q1 - Implement Queue Using Stacks.cpp
+++ Assignments/Solutions/Trijal Bhardwaj/Assignment No. 15 (Queue)/Q1 - Implement Queue Using Stacks.cpp
@@ -1,17 +1,51 @@
-// Question Link: https://leetcode.com/problems/two-sum/
+// Question Link: https://leetcode.com/problems/implement-queue-using-stacks/
 
-class Solution {
+class MyQueue {
   public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int>hash;
-        for (int i = 0; i < nums.size(); i++) {
-            int x = target - nums[i];
-            if (hash.find(x) != hash.end()) {
-                return{i, hash.find(x)->second};
-            } else {
-                hash[nums[i]] = i;
-            }
+    /** Initialize your data structure here. */
+    stack<int> input;
+    stack<int> output;
+    MyQueue() {
+    }
+
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        input.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        if (output.empty())
+            while (input.size())
+                output.push(input.top()), input.pop();
+
+        int x = output.top();
+        output.pop();
+        return x;
+    }
+
+    /** Get the front element. */
+    int peek() {
+        if (!output.empty()) {
+            return output.top();
         }
-        return {-1, -1};
+        while (!input.empty()) {
+            output.push(input.top());
+            input.pop();
+        }
+        return output.top();
+    }
+
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return (input.empty() && output.empty());
     }
 };
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
--- Assignments/Solutions/Trijal Bhardwaj/Assignment No. 15 (Queue)/Q2 - Design Circular Queue.cpp
+++ Assignments/Solutions/Trijal Bhardwaj/Assignment No. 15 (Queue)/Q2 - Design Circular Queue.cpp
@@ -1,25 +1,64 @@
-// Question Link: https://leetcode.com/problems/roman-to-integer/
+// Question Link: https://leetcode.com/problems/design-circular-queue/
 
-class Solution {
+class MyCircularQueue {
+  private:
+    int head, rear, SIZE, cnt;
+    vector<int> buffer;
+
   public:
-    int romanToInt(string s) {
-        map<char, int>m;
-        m.insert({'I', 1});
-        m.insert({'V', 5});
-        m.insert({'X', 10});
-        m.insert({'L', 50});
-        m.insert({'C', 100});
-        m.insert({'D', 500});
-        m.insert({'M', 1000});
-        int a = 0;
-        for (int i = 0; i < s.size(); i++) {
-            if (m[s[i]] < m[s[i + 1]]) {
-                a += m[s[i + 1]] - m[s[i]];
-                i++;
-                continue;
-            } else
-                a += m[s[i]];
-        }
-        return a;
+    MyCircularQueue(int k) : buffer(k, 0), head(0), rear(-1), SIZE(k), cnt(0) {
+    }
+
+    bool enQueue(int value) {
+        if (isFull())
+            return false;
+
+        rear = (rear + 1) % SIZE;
+        buffer[rear] = value;
+
+        cnt++;
+
+        return true;
+    }
+
+    bool deQueue() {
+        if (isEmpty())
+            return false;
+
+        head = (head + 1) % SIZE;
+        cnt--;
+
+        return true;
+    }
+
+    int Front() {
+        if (isEmpty())
+            return -1;
+        return buffer[head];
+    }
+
+    int Rear() {
+        if (isEmpty())
+            return -1;
+        return buffer[rear];
+    }
+
+    bool isEmpty() {
+        return cnt == 0;
+    }
+
+    bool isFull() {
+        return cnt == SIZE;
     }
 };
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue* obj = new MyCircularQueue(k);
+ * bool param_1 = obj->enQueue(value);
+ * bool param_2 = obj->deQueue();
+ * int param_3 = obj->Front();
+ * int param_4 = obj->Rear();
+ * bool param_5 = obj->isEmpty();
+ * bool param_6 = obj->isFull();
+ */
