51d35f4be8c011fa2d01b8a470bbc7412b22ee41
Trijal-Bhardwaj <Trijalbhardwaj00@gmail.com>
2021-08-13 06:08:32 +0530

    Assignment No. 14 (LinkedList) Solutions By TRIJAL BHARDWAJ

---
+++ Assignments/Solutions/Trijal Bhardwaj/Assignment No. 15 (Queue)/Q1 - Implement Queue Using Stacks.cpp
@@ -0,0 +1,17 @@
+// Question Link: https://leetcode.com/problems/two-sum/
+
+class Solution {
+  public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int>hash;
+        for (int i = 0; i < nums.size(); i++) {
+            int x = target - nums[i];
+            if (hash.find(x) != hash.end()) {
+                return{i, hash.find(x)->second};
+            } else {
+                hash[nums[i]] = i;
+            }
+        }
+        return {-1, -1};
+    }
+};
