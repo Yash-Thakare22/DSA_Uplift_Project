e69261f1313a5d08b7025d4ba81b93055b06f134
Trijal-Bhardwaj <Trijalbhardwaj00@gmail.com>
2021-08-13 07:54:29 +0530

    Assignment No. 17 (Greedy) Solutions By TRIJAL BHARDWAJ

---
+++ Assignments/Solutions/Trijal Bhardwaj/Assignment No. 17 (Greedy)/Q2 - Highest Product.cpp
@@ -0,0 +1,11 @@
+// Question Link: https://www.interviewbit.com/problems/highest-product/
+
+int Solution::maxp3(vector<int>& A) {
+    sort(A.begin(), A.end());
+    int n = A.size();
+    int x = 0;
+    int y = A[0] * A[1];
+    int w = A[n - 2] * A[n - 3];
+    x =  max(w * A[n - 1], y * A[n - 1]);
+    return x;
+}
