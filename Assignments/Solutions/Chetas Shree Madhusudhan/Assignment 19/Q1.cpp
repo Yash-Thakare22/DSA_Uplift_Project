5a56e184e18c18e34386d8d9b6980ef1b362f6d6
ChetasShree <cm7310@srmist.edu.in>
2021-08-19 12:00:43 +0530

    Assignment 19 trees

---
+++ Assignments/Solutions/Chetas Shree Madhusudhan/Assignment 19/Q1.cpp
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return max(left,right)+1;
+    }
+};
---
+++ Assignments/Solutions/Chetas Shree Madhusudhan/Assignment 19/Q4.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/delete-node-in-a-bst/
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root == NULL) return NULL;
+        if(root->val > key) root->left = deleteNode(root->left,key);
+        else if(root->val < key) root->right = deleteNode(root->right,key);
+        else{
+           // cout<<root->val<<endl;
+            if(root->left == NULL && root->right == NULL){
+                delete root;
+                return NULL;
+            }else if(root->left == NULL){
+                TreeNode* temp = root->right;
+                root->right = NULL;
+                delete root;
+                return temp;
+            }else if(root->right == NULL){
+                TreeNode* temp = root->left;
+                root->left = NULL;
+                delete root;
+                return temp;
+            }else{
+                TreeNode* temp = root->right;
+                while(temp->left !=NULL){
+                    temp = temp->left;
+                }
+                int minR = root->val;
+
+                swap(temp->val, root->val);
+                root->right = deleteNode(root->right,minR);
+            }
+        }return root;
+    }
+};
