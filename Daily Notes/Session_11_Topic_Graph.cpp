d7443089c677ae4bc2afe58bf304bdc4c5108d61
Swarnima Shishodia <swarnimashishodia11@gmail.com>
2021-08-25 21:34:12 +0530

    Update Session_11_Topic_Graph.cpp

--- Daily Notes/Session_11_Topic_Graph.cpp
+++ Daily Notes/Session_11_Topic_Graph.cpp
@@ -38,7 +38,7 @@ int main()
     return 0;
 }
 
-
+// TREE_ BFS
 void bfs(Node *root)
 {
     queue<Node*>q;
@@ -58,3 +58,71 @@ void bfs(Node *root)
         q.push(q->right);
     }
 }
+
+
+// GRAPH - BFS
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void bfs(vector<int>adj[], int nodes)
+{
+    queue<int>q;
+    q.push(0);
+    
+    int visited[nodes]={0};
+    visited[0]=1;
+    
+    while(q.empty()==false)
+    {
+        int f=q.front();
+        q.pop();
+        
+        cout<<f<<" ";
+        
+        for( auto x: adj[f])
+        {
+            if(visited[x]!=1)
+            {
+                q.push(x);
+                visited[x]=1;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int nodes,edges;
+    cin>>nodes;
+    cin>>edges;
+    
+    int u,v;
+    
+    vector<int>adj[nodes];
+    
+    for(int i=0;i<edges;i++)
+    {
+        cin>>u>>v;
+        adj[u].push_back(v); //grap[1][2]=1;
+        //adj[v].push_back(u); //graph[2][1]=1;
+    }
+    
+    for(int i=0;i<nodes;i++)
+    {
+        cout<<i<<"->";
+        for(auto x:adj[i])
+        cout<<x<<"->";
+        
+        cout<<endl;
+    }
+    
+    bfs(adj,nodes);
+    return 0;
+}
+
+
+// DFS GRAPH
+https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
+
