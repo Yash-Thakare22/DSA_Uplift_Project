a2fb22b598c52f66575504081cd469a5000db375
manika137 <80244229+manika137@users.noreply.github.com>
2021-08-28 19:37:17 +0530

    Q5.cpp

---
+++ Assignments/Solutions/Manika Joshi/Assignment14/Q5.cpp
@@ -0,0 +1,14 @@
+bool areIdentical(struct Node *head1, struct Node *head2)
+{
+    // Code here
+    while(head1!=NULL && head2!=NULL)
+	{
+	    if(head1->data!=head2->data)
+	        return false;
+	        
+	   head1=head1->next;
+	   head2=head2->next;
+	}
+	
+	return true;
+}
