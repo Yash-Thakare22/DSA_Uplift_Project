ada9f8833308e54ddbbe2b0a2ac46835449984b7
Trijal-Bhardwaj <Trijalbhardwaj00@gmail.com>
2021-09-06 19:12:33 +0530

    Assignment No. 22 (Heap) Solutions By TRIJAL BHARDWAJ

---
+++ Assignments/Solutions/Trijal Bhardwaj/Assignment No. 22 (Heap)/Q3 - Kth Smallest Element in a Sorted Matrix.cpp
@@ -0,0 +1,22 @@
+// Question Link : https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+
+class Solution {
+  public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        priority_queue<int,vector<int>,greater<int>>min;
+        for(int i=0; i<matrix.size(); i++) {
+            for(int j=0; j<matrix[i].size(); j++) {
+                min.push(matrix[i][j]);
+            }
+        }
+        int m=0;
+        while(!min.empty()) {
+            m++;
+            if(m==k) {
+                return min.top();
+            }
+            min.pop();
+        }
+        return -1;
+    }
+};
---
+++ Assignments/Solutions/Trijal Bhardwaj/Assignment No. 22 (Heap)/Q4 - Kth Largest Element in a Stream.cpp
@@ -0,0 +1,24 @@
+// Question Link: https://leetcode.com/problems/kth-largest-element-in-a-stream/
+
+class KthLargest {
+  public:
+    priority_queue<int,vector<int>,greater<int>>min;
+    int s;
+    KthLargest(int k, vector<int>& nums) {
+        s =  k;
+        for(int i=0; i<nums.size(); i++) {
+            min.push(nums[i]);
+            if(min.size()>k) {
+                min.pop();
+            }
+        }
+    }
+
+    int add(int val) {
+        min.push(val);
+        if(min.size()>s) {
+            min.pop();
+        }
+        return min.top();
+    }
+};
