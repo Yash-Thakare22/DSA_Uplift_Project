b0c2c61abb5685afabeaad9bfe555ca2e9e8e063
jasminekaur-prog <67196567+jasminekaur-prog@users.noreply.github.com>
2021-09-11 17:33:04 +0530

    Update Question 3.cpp

--- Assignments/Solutions/Jasmine Kaur/Assignment 2/Question 3.cpp
+++ Assignments/Solutions/Jasmine Kaur/Assignment 2/Question 3.cpp
@@ -1,26 +1,11 @@
 #include <iostream>
 using namespace std;
-int main()
-{ 
-  cout<<"Enter array size"<<endl;
-  int n,i,temp;
-  int arr[100];
-  cin>>n;
-  cout<<"Enter array"<<endl;
-	
-  for(i=0;i<n;i++)
-  { 
-    cin>>arr[i];}
-    temp=arr[n-1];
-  for(i=n-1;i>0;i--)
-  {
-    arr[i]=arr[i-1];  
-  }
-  arr[0]=temp;
+int main(){
+    int n=5;
+    int a[n] = {1,2,3,4,5};
+    int t = a[n-1];
+    for(int i=n-1;i>0;i--) a[i] = a[i-1];
+    a[0] = t;
+    for(int i=0;i<n;i++) cout<<a[i]<<" ";
 
-
-for(i=0;i<n;i++)
-		cout<<arr[i]<<"\t";
-	cout<<endl;	
-	return 0;
 }
