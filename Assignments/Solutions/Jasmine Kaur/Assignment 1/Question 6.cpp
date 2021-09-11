e3d7e1da47af54cf31fbed712cdd282d9b006376
jasminekaur-prog <67196567+jasminekaur-prog@users.noreply.github.com>
2021-09-11 17:12:01 +0530

    Create Question 6.cpp

---
+++ Assignments/Solutions/Jasmine Kaur/Assignment 1/Question 6.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+int count=n;
+for(int i=0;i<n;i++)
+{
+    for(int j=0;j<n-i;j++)
+    {
+   cout<<count;
+   count=count-1;
+    }
+    count=n;
+    cout<<endl;
+}
+return 0;
+}
