638c6e6f1bbf613598f655ab1c7c1f77740b9b8b
jasminekaur-prog <67196567+jasminekaur-prog@users.noreply.github.com>
2021-09-11 17:21:03 +0530

    Create Question 5.cpp

---
+++ Assignments/Solutions/Jasmine Kaur/Assignment 2/Question 5.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n=7;
+    int a[n] = {5 ,10 ,40 ,4, 6, 5, 10};
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            if(a[i] == a[j])
+            {
+              cout<<a[i]; 
+              return 0;
+            }
+        }
+    }
+    return 0;
+}
