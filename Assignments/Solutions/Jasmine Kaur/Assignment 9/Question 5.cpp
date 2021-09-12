3da103e44ec40ea39de8c21bfcd87d3f98a925e8
jasminekaur-prog <67196567+jasminekaur-prog@users.noreply.github.com>
2021-09-12 11:28:31 +0530

    Create Question 5.cpp

---
+++ Assignments/Solutions/Jasmine Kaur/Assignment 9/Question 5.cpp
@@ -0,0 +1,11 @@
+class Solution
+{
+public:
+    vector<int> decode(vector<int> &encoded, int first)
+    {
+        vector<int> ans{first};
+        for (int x : encoded)
+            ans.push_back(first ^= x);
+        return ans;
+    }
+};
