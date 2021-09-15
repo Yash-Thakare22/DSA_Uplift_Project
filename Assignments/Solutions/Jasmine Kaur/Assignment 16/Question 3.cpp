651d9d0f636dc15491edfb3454af199807a54cc9
jasminekaur-prog <67196567+jasminekaur-prog@users.noreply.github.com>
2021-09-15 16:09:59 +0530

    Create Question 3.cpp

---
+++ Assignments/Solutions/Jasmine Kaur/Assignment 16/Question 3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int>m1;
+        stack<int>s;
+        int n = nums2.size();
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() && s.top()<nums2[i]){
+                s.pop();
+            }
+            if(s.empty()) m1[nums2[i]] = -1;
+            else{
+                m1[nums2[i]] = s.top();
+            }s.push(nums2[i]);
+        }
+        vector<int>ans;
+        for(auto x:nums1){
+            ans.push_back(m1[x]);
+        }return ans;
+    }
+};
