ce0a52ce1e85c29b54ae30adf53bb4fd87787daf
jasminekaur-prog <67196567+jasminekaur-prog@users.noreply.github.com>
2021-09-15 17:29:53 +0530

    Update Question 1.cpp

--- Assignments/Solutions/Jasmine Kaur/Assignment 24/Question 1.cpp
+++ Assignments/Solutions/Jasmine Kaur/Assignment 24/Question 1.cpp
@@ -1,16 +1,14 @@
 class Solution {
-public:
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> B(amount+1,amount+1);
-        B[0]=0;
-        for(int i=0;i<coins.size();i++){
-            for(int j=coins[i];j<=amount;j++){
-                B[j] = min(B[j],B[j-coins[i]]+1);
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, amount+1);
+        dp[0] = 0;
+        
+        for(int coin : coins) {
+            for(int i = coin; i <=amount; i++){
+                dp[i] = Math.min(dp[i], dp[i-coin]+1);
             }
         }
-        
-        if(B[amount]==amount+1)
-            return -1;
-        return B[amount];
+        return dp[amount] <= amount ? dp[amount] : -1;
     }
-};
+}
