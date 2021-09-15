30f04fcdcd2badbe0354074a67283dada2f6403f
jasminekaur-prog <67196567+jasminekaur-prog@users.noreply.github.com>
2021-09-15 16:27:55 +0530

    Create Question 1.cpp

---
+++ Assignments/Solutions/Jasmine Kaur/Assignment 20/Question 1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int uniquePaths(int m, int n) {
+        int arr[m][n];
+        for(int i=0; i<m; i++) {
+            arr[i][0] =1;
+        }
+        for(int i=0; i<n; i++) {
+            arr[0][i] =1;
+        }
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                arr[i][j] = arr[i-1][j]+arr[i][j-1];
+            }
+        }
+        return arr[m-1][n-1];
+    }
+};
