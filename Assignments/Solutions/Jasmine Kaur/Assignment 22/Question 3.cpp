dc275ea6ffc86d5b91e6d886bb40ad6fd3d37068
jasminekaur-prog <67196567+jasminekaur-prog@users.noreply.github.com>
2021-09-15 16:44:24 +0530

    Create Question 3.cpp

---
+++ Assignments/Solutions/Jasmine Kaur/Assignment 22/Question 3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        priority_queue<int,vector<int>,greater<int>>min;
+        for(int i=0; i<matrix.size(); i++) {
+            for(int j=0; j<matrix[i].size(); j++) {
+                min.push(matrix[i][j]);
+            }
+        }
+        int m=0;
+        while(!min.empty()) {
+            m++;
+            if(m==k) {
+                return min.top();
+            }
+            min.pop();
+        }
+        return -1;
+    }
+};
