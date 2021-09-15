78c4d6131857a14c2855d01b11715085499e56d3
jasminekaur-prog <67196567+jasminekaur-prog@users.noreply.github.com>
2021-09-15 15:32:57 +0530

    Update Question 2.cpp

--- Assignments/Solutions/Jasmine Kaur/Assignment 12/Question 2.cpp
+++ Assignments/Solutions/Jasmine Kaur/Assignment 12/Question 2.cpp
@@ -1,7 +1,11 @@
-int Solution::trailingZeroes(int A) {
-    int x=0;
-    while(A/5>0){
-        x+=(A/5);
-        A/=5;
-    }return x;
+int Solution::coverPoints(vector<int> &x, vector<int> &y) {
+    int n = x.size(), ans = 0;
+    for (int i = 0; i < n - 1; i++) {
+        if (abs(x[i] - x[i + 1]) <= abs(y[i] - y[i + 1]))
+            ans += abs(y[i] - y[i + 1]);
+        else {
+            ans += abs(x[i] - x[i + 1]);
+        }
+    }
+    return ans;
 }
