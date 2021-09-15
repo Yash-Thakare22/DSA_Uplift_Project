fbaaa190eb584bc5a8826df62cf77fb823d5183e
jasminekaur-prog <67196567+jasminekaur-prog@users.noreply.github.com>
2021-09-15 15:33:31 +0530

    Update Question 3.cpp

--- Assignments/Solutions/Jasmine Kaur/Assignment 12/Question 3.cpp
+++ Assignments/Solutions/Jasmine Kaur/Assignment 12/Question 3.cpp
@@ -1,39 +1,7 @@
-int rc(char a){
-    switch(a){
-        case 'X':
-            return 10;
-        case 'I':
-            return 1;
-        case 'V':
-            return 5;
-        case 'L':
-            return 50;
-        case 'M':
-            return 1000;
-        case 'C':
-            return 100;
-        case 'D':
-            return 500;
-        default:
-            return 0;
-
+int Solution::titleToNumber(string A) {
+    int x = 0;
+    for (int i = 0; i < A.size(); i++) {
+        x = x * 26 + (A[i] - 'A' + 1);
     }
-}
-
-int Solution::romanToInt(string A) {
-    int x=0;
-    for(int i=0;i<A.size();i++){
-        int j=A[i],k=0;
-        if(i<A.size()-1) k=A[i+1];
-        if(rc(A[i+1])>rc(A[i]) && i<A.size()-1){
-            x+=rc(A[i+1]);
-            x-=rc(A[i]);
-            i++;
-        }else{
-            x+=rc(A[i]);
-        }
-        
-    }return x;
-
-
+    return x;
 }
