b0ac2918bb50f130da7ee77794386c8e118bf249
jasminekaur-prog <67196567+jasminekaur-prog@users.noreply.github.com>
2021-09-15 15:57:10 +0530

    Create Question 1.cpp

---
+++ Assignments/Solutions/Jasmine Kaur/Assignment 14/Question 1.cpp
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+
+}*start;
+
+void insert();
+void display(struct Node *head);
+
+int main() {
+    int t;
+    scanf("%d",&t);
+    while(t--) {
+        start=NULL;
+        insert();
+        printf("%d",getCount(start));
+        printf("\n");
+    }
+    return 0;
+
+}
+
+
+void insert() {
+    int n,value,i;
+    scanf("%d",&n);
+    struct Node *temp;
+    for(i=0; i<n; i++) {
+        scanf("%d",&value);
+        if(i==0) {
+            start=(struct Node *) malloc( sizeof(struct Node) );
+            start->data=value;
+            start->next=NULL;
+            temp=start;
+            continue;
+        } else {
+            temp->next= (struct Node *) malloc( sizeof(struct Node) );
+            temp=temp->next;
+            temp->data=value;
+            temp->next=NULL;
+        }
+    }
+}
+
+int getCount(struct Node* head) {
+    int count=0;
+    while(head!=NULL) {
+        count++;
+        head=head->next;
+    }
+    return count;
+}
